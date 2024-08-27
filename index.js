1408d2314e144d0853c8f96c1e8dac3e78ceba95
dakshthawani <dakshthawani70@gmail.com>
2024-08-28 02:48:07 +0530

    second commit

--- index.js
+++ index.js
@@ -28,14 +28,72 @@ app.get("/filter", (req,res) =>{
 } )
  
 //4. POST a new joke
+app.post("/jokes", (req,res) => {
+  const newJoke = {
+    id: jokes.length + 1,
+    jokeText: req.body.text,
+    jokeTyoe: req.body.type
+  }
+  jokes.push(newJoke)
+  console.log(jokes.slice(-1));
+  res.json(newJoke)
+  
+})
 
 //5. PUT a joke
+app.put("/jokes/:id", (req,res) => {
+  const id = parseInt(req.params.id)
+  const replacementJoke = {
+    id: id,
+    jokeText: req.body.text,
+    jokeType: req.body.type
+  }
+  const searchIndex = jokes.findIndex(joke => joke.id==id)
+  jokes[searchIndex] = replacementJoke
+  res.json(replacementJoke)
+})
 
 //6. PATCH a joke
+app.patch("/jokes/:id", (req,res) => {
+  const id = parseInt(req.params.id)
+  const existingJoke = jokes.find(joke=>joke.id===id)
+  const replacementJoke = {
+    id: id,
+    jokeText: req.body.text || existingJoke.jokeText,
+    jokeType: req.body.type || existingJoke.jokeType
+  }
+  const searchIndex = jokes.findIndex(joke => joke.id==id)
+  jokes[searchIndex] = replacementJoke
+  res.json(replacementJoke)
+})
 
 //7. DELETE Specific joke
-
+app.delete("/jokes/:id", (req,res) => {
+  const id = parseInt(req.params.id)
+  const searchIndex = jokes.findIndex(joke => joke.id==id)
+  if (searchIndex > -1){
+    jokes.splice(searchIndex,1);
+    res.sendStatus(200)
+  }
+  else{
+    res
+      .sendStatus(404)
+      .json("Error")
+  }
+})
 //8. DELETE All jokes
+app.delete("/all", (req,res) => {
+  const userKey = req.query.key
+  if (userKey == masterKey){
+    jokes = []
+    res.sendStatus(200)
+  }
+  else{
+    res
+      .sendStatus(404)
+      .json("Error")
+  }
+})
 
 app.listen(port, () => {
   console.log(`Successfully started server on port ${port}.`);
